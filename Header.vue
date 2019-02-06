35ab3587b9849f2c33fd9a8732eeb5b24b3a73ef
mmnmtr <38813737+mmnmtr@users.noreply.github.com>
2019-02-07 00:30:02 +0700

    Add files via upload

---
+++ Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nunito">
+      <b-navbar toggleable type="light" variant="light">
+          <b-navbar-toggle target="nav_text_collapse" class="none"></b-navbar-toggle>
+
+          <b-navbar-brand href="/"><b-img width="70" blank-color="#8dc448" alt="center img" class="m-1" src="https://www.img.in.th/images/006349ed854978bfd11bd0e2c2025af7.png" /></b-navbar-brand>
+
+          
+
+          <b-collapse is-nav id="nav_text_collapse">
+              <b-navbar-nav>
+                  <b-nav-item href="otpnumber"> OTP NUMBER</b-nav-item>
+                  <b-nav-item href="artlist"> ARTIST </b-nav-item>
+                  <b-nav-item href="vote"> VOTE </b-nav-item>
+                  <b-nav-item href="post"> POST </b-nav-item>
+                  <!-- <b-nav-item href="home"> HOME </b-nav-item> -->
+              </b-navbar-nav>
+          </b-collapse>
+      </b-navbar>
+  </div>
+</template>
---
+++ Thank.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<app-header></app-header>
+	<meta content="width=device-width,initial-scale=1" name="viewport">
+    <link href="https://fonts.googleapis.com/css?family=Kanit|Nunito:900" rel="stylesheet">
+    <div>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<b-img src="https://brandinside.asia/wp-content/uploads/2016/08/AIS-logo-1.jpg" fluid alt="Fluid image" />
+    	<h2 class="text-center nunito" style="color:#8dc448;"> THANK YOU </h2>
+    	<h4 class="text-center kanit"> " ขอขอบคุณการร่วมโหวตผลงาน <br> ศิลปะไทยของคนไทยในครั้งนี้ " </h4>
+    </div>
+	</div>
+</template>
+<script>
+	import Header from './Header.vue'
+	export default{
+		components: {
+			appHeader: Header,
+		}
+	}
+</script>
